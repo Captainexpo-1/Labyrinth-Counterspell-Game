c1ba29675cdd8b0bc42d9e3f78820e96778dd8df
Elliot <captainexpo@gmail.com>
2024-11-23 14:49:37 -0500

    Fixed viewport stretching

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -6,6 +6,7 @@ var current_state
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	self.current_state = states.PAUSED
 
 
